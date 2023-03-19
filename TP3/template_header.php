c8cab61c4a89d5daec640fa313f3285f8050bcbc
TheDadi59 <di.danielnd59860@gmail.com>
2023-03-19 12:50:28 +0100

    Debut tp3

---
+++ TP3/template_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Ma page descriptive</title>
+        <?php 
+            if(isset($_COOKIE['style'])){
+                echo "<link rel='stylesheet' href='css/{$_COOKIE["style"]}.css'>";
+            }else{
+                echo "<link rel='stylesheet' href='css/style1.css'>";
+            }
+        ?>
+    </head>
